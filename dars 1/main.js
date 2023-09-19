c8150d4e17734585a17542ee2650ca0b08de3654
behzodbaxtiyorov <133630500+behzodbaxtiyorov@users.noreply.github.com>
2023-09-19 11:33:39 +0500

    Add files via upload

---
+++ dars 1/main.js
@@ -0,0 +1,206 @@
+
+// 1-savol javascript nima? 
+// Javascript- dunyodagi eng ommabop va keng tatbiq etiladigan veb-dasturlash tilidir.
+
+
+
+
+// 2-savol JavaScriptni HTMLga boglash?
+// JavaScriptni HTMLga  'SCRIPT' tegi orqali bohlanadi
+
+
+
+// 3-savol matematik operatorlar?
+// +, -, =, *, /, v/h
+
+
+
+
+
+// 4-savol Variables haqida?
+// O'zgaruvchilar asosan uch turga bolinadi ular: 'var' 'let' 'const'
+
+
+// Var-
+// var age =21;
+// var name = 'Behzod';
+// console.log(age);
+// console.log(name);
+
+
+
+// Let-let bilan chaqirilgan ozgaruvchilar block skobkalar ichida boladi va blockdan tashqariga cjiqmaydi
+// {
+//     let x =2;
+//     console.log(x);
+// }
+
+
+
+
+
+// const- const bilan elon qilingan uzgaruvchilar bitta qiymat beriladi va uni uzgartirib bolmaydi
+
+
+
+
+
+
+
+
+// 5-savol Data types?
+// Data typeslar 9 turga bolinadi shulardan 7 tasi bizga malum bular: 'string'; 'number'; 'boolean'; 'null'; 'undefined'; 'object'; 'array';
+
+
+
+
+
+
+// String- stringlar har doim qoshtirnoq ichida yoziladigan textlar
+// var person = "Behzod"
+// console.log(person);
+
+
+
+
+// Number- Sonlar 
+// let age = 21
+// console.log(age);
+
+
+
+
+
+// Boolean- 'true' va 'false'
+
+
+
+
+
+// Null- null 'hech narsa ' degani. Null qiymat mavjud emasligini anglatadi
+
+
+
+
+
+
+
+
+// undefined- o'zgaruvchi elon qilingan lekin biror narsaga tenglashtirimagan bolsa undefined (qiymat berilmagan) boladi. masalan:
+
+// var x;
+// console.log(x);   //undefined
+
+
+
+
+
+
+// Object- eng murakkab va mukammal tur hisoblanadi va u uz ichiga bir nechta qiymatlarni qabul qila oladi. Masalan:
+
+// let person = {
+//     name: 'Behzod',
+//     age: 21,
+//     erkak: true
+// };
+// console.log(person.age);
+
+
+
+
+
+
+
+// if else
+
+// let a = 10;
+
+// if (a > 0) {
+//     console.log('0 dan katta');
+// } else {
+//     console.log('0 dan kichik');
+// }
+
+
+
+
+
+
+
+
+// logical operatorlar- bu operatorlar qiymatlarning 'true' yoli 'false' bolishini tekshiradi. masalan:
+// && 'va' ga misol
+// let x = 10;
+// let y = 30;
+
+// if (x > 5 && y < 35) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+
+
+
+// || 'yoli' ga misol
+
+// let x = 10;
+// let y = 30;
+
+// if (x > 15 || y < 32) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+
+
+
+
+
+
+
+
+
+
+// type conversions - bir malumot turidan boshqa malumotturiga utkazish
+
+// let x = '10';
+// let y = 5;
+
+// let res = x + y;
+// console.log(res); 
+
+// let res = x - y;
+// console.log(res);
+
+
+
+
+
+
+
+
+
+
+// ternary operatorlar
+
+// let age = 21;
+
+// let PravaOlish = age >= 18 ? 'prava oladi' : 'prava ololmaydi';
+// console.log(PravaOlish);
+
+
+
+// var a = 7;
+// var result = a>0 ? "true" : "false";
+// console.log(result);
+
+
+
+
+// var a = 9;
+// var b = 5;
+// var c = 4;
+// var result = a>b && b>c ? "true" : "false";
+// console.log(result);
