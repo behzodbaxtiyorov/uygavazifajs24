dc87ec6738591ababca3f7ddd3317bc700230775
behzodbaxtiyorov <133630500+behzodbaxtiyorov@users.noreply.github.com>
2023-09-19 15:34:46 +0500

    Add files via upload

---
+++ dars 2/main.js
@@ -0,0 +1,425 @@
+
+// 1-masala for
+
+// let k = 20;
+// let n = 7;
+
+// function loopNum(k, n) {
+//     for (let i = 0; i < n; i++){
+//         console.log(k);
+//     }
+// } 
+
+// loopNum(k, n);
+
+
+
+
+
+
+
+
+
+
+
+
+
+//2-masala for
+
+// let a = 20;
+// let b = 10;
+
+// function num(a, b) {
+//     if(a > b) {
+//         for (let i = a; i >= b; i--) {
+//             console.log(i);
+//             }
+//     }else{
+//         console.log('ishlamaydi');
+//     }
+// }
+// num(a, b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 3-masala for
+
+
+// let x = 16;
+// let y = 8;
+
+// function son(x, y) {
+//     if(x > y) {
+//         for (let i = 15; i > y; i--) {
+//             console.log(i);
+//             }
+//     }else{
+//         console.log('ishlamaydi');
+//     }
+// }
+// son(x, y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 4-masala for
+
+// let konfet = 10000;
+
+// function konfetPrise(){
+//     for (let i = 1; i <= 10; i++){
+//         console.log(konfet * i);
+//     } 
+// }
+
+// konfetPrise();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 5-masala for
+
+// let newKonfet = 10000;
+
+// function konfetP(){
+//     for (let i = 0.1; i <= 1; i += 0.1) {
+//         let price = i * 10000;
+//         console.log(i + " kg konfet narxi: " + 10000 + " so'm");
+//       }
+// }
+// konfetP();
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 6-masala for
+
+// let konfetlar = 10000;
+
+// function konf(){
+//     for (let i = 1.2; i <= 2; i += 0.2) {
+//         let price = i * 10000;
+//         console.log(i + " kg  narxi: " + price + " so'm");
+//       }
+// } 
+// konf();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 7-masala for
+
+// let a = 1;
+// let b =5;
+
+
+// function yigindiTop(a, b) {
+//     let sum = 0
+//     for (let i = a; i <= b; i++) {
+//       sum += i;
+//     }
+//     return sum
+//   }
+
+
+// let res = yigindiTop(a, b);
+// console.log( res);
+
+
+
+
+
+
+
+
+
+
+
+
+// 8-masala for
+
+// let a = 1;
+// let b = 7;
+
+// function kopaytmaTop(a, b) {
+//     let product = 1;
+//     for (let i = a; i <= b; i++) {
+//       product *= i;
+//     }
+//     return product;
+//   }
+  
+//   let result = kopaytmaTop(a, b);
+//   console.log( result);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+// 9-masala for
+
+// let a = 1;
+// let b = 5;
+
+// function kvadrat(a, b) {
+//     let sum = 0;
+//     for (let i = a; i <= b; i++) {
+//       sum += i * i;
+//     }
+//     return sum;
+//   }
+  
+//   let result = kvadrat(a, b);
+//   console.log( result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 10-masala for
+
+// let n = 20;
+
+// function toqSonlar(n){
+//     for (let i = 1; i < n; i +=2){
+//          console.log(i);
+//     }
+// }
+// toqSonlar(n);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 11-masala for
+
+// let n = 10;
+
+// function juftSonlar(n){
+//     for (let i = 2; i < n; i +=2){
+//         console.log(i);
+//     }
+// }
+// juftSonlar(n);
+
+
+
+
+
+
+
+
+
+
+
+// 12-masala for
+
+
+// let n = 20;
+
+// function murakkabSonalr(n) {
+//     for (let i = 2; i <= n; i++) {
+//       let isMurakkab = true;
+//       for (let j = 2; j < i; j++) {
+//         if (i % j === 0) {
+//           isMurakkab = false;
+//           break;
+//         }
+//       }
+//       if (isMurakkab) {
+//         console.log(i);
+//       }
+//     }
+//   }
+
+//   murakkabSonalr(n);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 13-masala for
+
+
+// let n = 10;
+
+
+// function juftToq(n) {
+//     for (let i = 1; i <= n; i++) {
+//       if (i % 2 === 0) {
+//         console.log(i + '  kvadrati: ' + i * i);
+//       } else {
+//         console.log(i + ' toq son');
+//       }
+//     }
+//   }
+//   juftToq(n);
+
+
+
+
+
+
+
+
+
+
+
+
+// 14-masala for
+
+// let n = 5;
+
+// function kamayish(n) {
+//     for (let i = n; i >= 1; i--) {
+//       console.log(i);
+//     }
+// }    
+
+// kamayish(n);
+
+
+
+
+
+
+
+
+
+
+
+
+// 15-masala for
+
+// let n = 10;
+
+// function tashlab(n) {
+//     for (let i = n; i >= 0; i--) {
+//       if (i === 5) {
+//         console.log('Tashlab otildi!');
+//         break;
+//       }
+//       console.log(i);
+//     }
+//   }
+//   tashlab(n);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 16-masala for
+
+// let n = 10;
+
+// function kopaytma(n) {
+//     let kopaytma = 1;
+//     for (let i = 1; i <= n; i++) {
+//       kopaytma *= i;
+//     }
+//     console.log( kopaytma);
+//   }
+  
+//   kopaytma(n);
