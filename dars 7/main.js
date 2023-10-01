5c9bb9d471948e782d585ee7d885d9cd46af66c2
behzodbaxtiyorov <133630500+behzodbaxtiyorov@users.noreply.github.com>
2023-10-01 18:50:11 +0500

    Add files via upload

---
+++ dars 7/main.js
@@ -0,0 +1,439 @@
+// class Car {
+//     constructor(name, pozisiya, yili, narxi){
+//         this.name = name;
+//         this.pozisiya = pozisiya;
+//         this.yili = yili;
+//         this.narxi = narxi;
+//     }
+//     getName(){
+//         console.log(this.name);
+//     }
+
+//     getPozisiya(){
+//         console.log(this.pozisiya);
+//     }
+
+//     getYili(){
+//         console.log(this.yili);
+//     }
+
+//     getNarxi(){
+//         console.log(this.narxi);
+//     }
+// }
+
+// let gentra = new Car('gentra',4,2021,25000);
+
+
+
+// let vaqt = new Date();
+// let soat = vaqt.getHours() ;
+// let minut = vaqt.getMinutes();
+
+
+
+
+
+
+
+
+// localStorage.setItem( 'name', 'ali')
+
+// console.log(localStorage.getItem( 'name'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// uyga vazifa 
+
+
+// class Keyboard {
+//   constructor(number, functions) {
+//     this.number = number;
+//     this.functions = functions;
+//   }
+// }
+
+// const esc = new Keyboard(1, "Quit all files, back to previous file");
+
+// const f1 = new Keyboard(2, "Opens the Help system for the active window");
+
+// const f2 = new Keyboard(3, "Light down");
+
+// const f3 = new Keyboard(4, "Light up");
+
+// const f4 = new Keyboard(5, "Creates a new document in programs that support this command");
+
+// const f5 = new Keyboard(6, "Refresh");
+
+// const f6 = new Keyboard(7, "No volume");
+
+// const f7 = new Keyboard(8, "Volume down");
+
+// const f8 = new Keyboard(9, "Volume Up");
+
+// const f9 = new Keyboard(10, "Play previous media file");
+
+// const f10 = new Keyboard(11, "play and pause media file");
+
+// const f11 = new Keyboard(12, "play next media file");
+
+// const f12 = new Keyboard(13, "Pilot mode");
+
+// const prt_sc = new Keyboard(14, "Make screenshot of screen");
+
+// const del = new Keyboard(15, "delete anything");
+
+// const home = new Keyboard(16, "auto go to beginning of row");
+
+// const end = new Keyboard(17, "auto go to end of row");
+
+// const pg_up = new Keyboard(18, "auto go to beginning of page");
+
+// const pg_down = new Keyboard(19, "auto go to end of page");
+
+// const Backtick = new Keyboard(20, "write symbol of ` or ~ ");
+
+// const num1 = new Keyboard(21, "Write 1 or ! ");
+
+// const num2 = new Keyboard(22, "write 2 or @ ");
+
+// const num3 = new Keyboard(23, "write 3 or # ");
+
+// const num4 = new Keyboard(24, "write 4 or $ ");
+
+// const num5 = new Keyboard(25, "write 5 or % ");
+
+// const num6 = new Keyboard(26, "write 6 or ^ ");
+
+// const num7 = new Keyboard(27, "write 7 or & ");
+
+// const num8 = new Keyboard(28, "write 8 or * ");
+
+// const num9 = new Keyboard(29, "write 9 or ( ");
+
+// const num0 = new Keyboard(30, "write 0 or ) ");
+
+// const minus = new Keyboard(31, "write - or _ ");
+
+// const equals = new Keyboard(32, "write = or + ");
+
+// const Backspace = new Keyboard(33, "The Backspace button is used to delete the character before the cursor or the selected item.");
+
+
+// const numLock = new Keyboard(34, "block the ability of writing numbers on num pad");
+
+// const slash = new Keyboard(35, "Write / ");
+
+// const Asterisk = new Keyboard(36, "Write * ");
+
+// const minus2 = new Keyboard(37, "Write - ");
+
+// const tab = new Keyboard(38, "Moves the cursor or focus to the next selectable element. ");
+
+
+// const q = new Keyboard(39, "Write q ");
+
+// const w = new Keyboard(40, "Write w ");
+
+// const e = new Keyboard(41, "Write e ");
+
+// const r = new Keyboard(42, "Write r ");
+
+// const t = new Keyboard(43, "Write t ");
+
+// const y = new Keyboard(44, "Write y ");
+
+// const u = new Keyboard(45, "Write u ");
+
+// const i = new Keyboard(46, "Write i ");
+
+// const o = new Keyboard(47, "Write o ");
+
+// const p = new Keyboard(48, "Write p ");
+
+// const leftBracket = new Keyboard(49, "Write { or [ ");
+
+// const rightBracket = new Keyboard(50, "Write } or ] ");
+
+// const backslash = new Keyboard(51, "Write \ or |  "); 
+
+// const numpad7 = new Keyboard(52, "Write 7 or function Home in num lock ");
+
+// const numpad8 = new Keyboard(53, "Write 8  ");
+
+// const numpad9 = new Keyboard(54, "Write 9 or function Page up in num lock ");
+
+// const capslock = new Keyboard(55, "Write all letters in capital ");
+
+// const a = new Keyboard(56, "Write a ");
+
+// const s = new Keyboard(57, "Write s ");
+
+// const d = new Keyboard(58, "Write d ");
+
+// const f = new Keyboard(59, "Write f ");
+
+// const g = new Keyboard(60, "Write g ");
+
+// const h = new Keyboard(61, "Write h ");
+
+// const j = new Keyboard(62, "Write j ");
+
+// const k = new Keyboard(63, "Write k ");
+
+// const l = new Keyboard(64, "Write l ");
+
+// const Semicolon = new Keyboard(65, "Write ; or : ");
+
+// const Apostrophe = new Keyboard(66, "Write ' or " );
+
+// const enter = new Keyboard(67, 'submit anything');
+
+
+// const numpad4 = new Keyboard(68, "Write 4  ");
+
+// const numpad5 = new Keyboard(69, "Write 5  ");
+
+// const numpad6 = new Keyboard(70, "Write 6 ");
+
+// const numpadplus = new Keyboard(71, "Write + ");
+
+// const leftshift = new Keyboard(72, "The Shift button is used to type capital letters, access symbols on the upper part of keys, or activate secondary functions of certain keys.");
+
+
+
+// const z = new Keyboard(73, "Write d ");
+
+// const x = new Keyboard(74, "Write f ");
+
+// const c = new Keyboard(75, "Write g ");
+
+// const v = new Keyboard(76, "Write h ");
+
+// const b = new Keyboard(77, "Write j ");
+
+// const n = new Keyboard(78, "Write k ");
+
+// const m = new Keyboard(79, "Write l ");
+
+// const comma = new Keyboard(80, "Write , or < ");
+
+// const period = new Keyboard(81, "Write . or > ");
+
+// const question  = new Keyboard(82, "Write ? or / ");
+
+// const rightshift = new Keyboard(83, "The Shift button is used to type capital letters, access symbols on the upper part of keys, or activate secondary functions of certain keys.");
+
+
+
+// const numpad1 = new Keyboard(84, "Write 1 or funtion end in num lock  ");
+
+// const numpad2 = new Keyboard(85, "Write 2");
+
+// const numpad3 = new Keyboard(86, "Write 3 or  funtion Page down in num lock ");
+
+
+
+// const leftctrl = new Keyboard(87, " The Ctrl (Control) button is used as a modifier key in combination with other keys to perform various shortcuts and commands within an application or operating system.");
+
+// const rightctrl = new Keyboard(88, " The Ctrl (Control) button is used as a modifier key in combination with other keys to perform various shortcuts and commands within an application or operating system.");
+
+// const fn = new Keyboard(89, " button is used to activate secondary functions or special features assigned to specific keys on a laptop keyboard. It allows you to access additional functions such as adjusting screen brightness, volume control, multimedia playback, and other specialized commands.");
+
+
+// const windows = new Keyboard(90, "button (Win) is used to access the Start menu or Start screen, open the taskbar, and navigate through the operating system on Windows-based laptops. It provides quick access to system features, search functionality, and application shortcuts.");
+
+
+
+// const leftalt = new Keyboard(91, "button is used as a modifier key in combination with other keys to access alternate characters, trigger shortcuts, or activate specific functions within an application.");
+
+
+// const space= new Keyboard(92, " put space between any objects ");
+
+
+// const rightalt = new Keyboard(93, "button is used as a modifier key in combination with other keys to access alternate characters, trigger shortcuts, or activate specific functions within an application.");
+
+// const left = new Keyboard ( 94 , 'move cursor to left')
+
+// const up= new Keyboard ( 95 , 'move cursor to up')
+
+// const down= new Keyboard ( 96 , 'move cursor to down')
+
+// const right = new Keyboard ( 97 , 'move cursor to right')
+
+
+// const numpad0 = new Keyboard(98, "Write 0 ");
+
+// const numpaddel = new Keyboard(99, "Write . or delete function in num lock ");
+
+// const numpadenter = new Keyboard(100, 'submit anything');
+
+// const power = new Keyboard(101, 'power on and off of laptop');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  BU OZGINA BOSHQACHA YECHIM USTOZ
+
+
+
+
+// class Keyboard {
+//     constructor(name, type){
+//         this.name = name;
+//         this.type = type;
+//     }
+//     // 1
+//     esc(){
+//         console.log('esc bosilganda smth happen');
+//     }
+//     // 2
+//     shift(){
+//         console.log('shift bosilganda smth happen');
+//     }
+//     // 3
+//     tab(){
+//         console.log('tab bosilganda smth happen');
+//     }
+//     // 4
+//     capslk(){
+//         console.log('capslk bosilganda smth happen');
+//     }
+//     // 5
+//     ctrl(){
+//         console.log('ctrl bosilganda smth happen');
+//     }
+//     // 6
+//     fn(){
+//         console.log('fn bosilganda smth happen');
+//     }
+//     // 7
+//     windows(){
+//         console.log('windows bosilganda smth happen');
+//     }
+//     // 8
+//     alt(){
+//         console.log('alt bosilganda smth happen');
+//     }
+//     // 9
+//     probel(){
+//         console.log('probel bosilganda smth happen');
+//     }
+//     // 10
+//     home(){
+//         console.log('home bosilganda smth happen');
+//     }
+//     // 11
+//     pgup(){
+//         console.log('pgup bosilganda smth happen');
+//     }
+//     // 12
+//     pgdn(){
+//         console.log('pgdn bosilganda smth happen');
+//     }
+//     // 13
+//     end(){
+//         console.log('end bosilganda smth happen');
+//     }
+//     // 14
+//     enter(){
+//         console.log('enter bosilganda smth happen');
+//     }
+//     // 15
+//     del(){
+//         console.log('del bosilganda smth happen');
+//     }
+//     // 16
+//     numlock(){
+//         console.log('numlock bosilganda smth happen');
+//     }
+//     // 17
+//     backspace(){
+//         console.log('backspace bosilganda smth happen');
+//     }
+//     // 18
+//     delete(){
+//         console.log('delete bosilganda smth happen');
+//     }
+//     // 19
+//     insert(){
+//         console.log('insert bosilganda smth happen');
+//     }
+//     // 20
+//     prtsc(){
+//         console.log('prtsc bosilganda smth happen');
+//     }
+//     // 21
+//     f1(){
+//         console.log('f1 bosilganda smth happen');
+//     }
+//     // 22
+//     f2(){
+//         console.log('f2 bosilganda smth happen');
+//     }
+//     // 23
+//     f3(){
+//         console.log('f3 bosilganda smth happen');
+//     }
+//     // 24
+//     f4(){
+//         console.log('f4 bosilganda smth happen');
+//     }
+//     // 25
+//     f5(){
+//         console.log('f5 bosilganda smth happen');
+//     }
+//     // 26
+//     f6(){
+//         console.log('f6 bosilganda smth happen');
+//     }
+//     // 27
+//     f7(){
+//         console.log('f7 bosilganda smth happen');
+//     }
+//     // 28
+//     f8(){
+//         console.log('f8 bosilganda smth happen');
+//     }
+//     // 29
+//     f9(){
+//         console.log('f9 bosilganda smth happen');
+//     }
+//     // 30
+//     f10(){
+//         console.log(('f10 bosilganda smth happen'));
+//     }
+//     // 31
+//     f11(){
+//         console.log('f11 bosilganda smth happen');
+//     }
+//     // 32
+//     f12(){
+//         console.log('f12 bosilganda smth happen');
+//     }
+// }
+
+// const key = new Keyboard('magic', 'vireless')
